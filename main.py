5bedadcf8ae8bae22c15fdce9273bca4375d9d77
JOAO VICTOR DOS SANTOS ROCHA <joao.vsr1@puccampinas.edu.br>
2024-04-19 18:55:01 -0300

    Adição da biblioteca tabulate para exibição da tabela

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from tabulate import tabulate
+
 def obter_input(mensagem):
     valor = input(mensagem)
     while not valor.strip():
@@ -39,41 +41,22 @@ while True:
             #Fórmula Preço de Venda
             PV = CP / (1 - ((CF + CV + IV + ML) / 100))
 
-            #Descrição
-            #A. Preço de Venda(PV)
-            print('O Preço de venda foi de ', round(PV), 'que é igual a 100% do valor final')
-            
-            #B. Custo de Aquisição (Fornecedor)
-            porcent = CP * 100 / PV
-            print("o preço do produto pelo fornecedor foi igual a", round(CP),' que é igual a', round(porcent), '% do valor final')
-           
-            #C. Receita Bruta (A-B)
-            bruto = PV - CP
-            porcent1 = (bruto * 100) / PV
-            print('a receita bruta foi de ', round(bruto), 'que é igual a', round(porcent1),'% do valor final')
-            
-            #D. Custo Fixo/Administrativo
-            valorCF = PV * CF / 100
-            print('o custo fixo foi de ', round(valorCF), 'que é igual a', round(CF),'% do valor final')
-            
-            #E. Comissão de Vendas
-            valorCV = PV * CV / 100
-            print('a comissao foi de ', round(valorCV), 'que é igual a', round(CV),'% do valor final')
-            
-            #F. Impostos
-            valorIV = PV * IV / 100
-            print('o valor do imposto foi de ', round(valorIV), 'que é igual a', round(IV),'% do valor final')
-            
-            #G. Outros custos (D+E+F)
-            resto = valorCF + valorCV + valorIV
-            porcent2 = CF + CV + IV
-            print('outros custos ', round(resto), 'que é igual a', round(porcent2),'% do valor final')
-            
-            #H. Rentabilidade (C-G)
-            rentabilidade = bruto - resto
-            print('a rentabilidade foi de', round(rentabilidade), 'que é igual a', round(ML), '% do valor final')
+            print()
+            tabela_cabecalho = ["DESCRIÇÃO", "VALOR", "%"]
+            tabela_resultados = [
+                ["A. Preço de Venda", PV, "100"],
+                ["B. Custo de Aquisição (Fornecedor)", CP, (CP / PV) * 100],
+                ["C. Receita Bruta (A-B)", (PV - CP), ((PV - CP) / PV) * 100],
+                ["D. Custo Fixo/Administrativo", (PV * CF) / 100, CF],
+                ["E. Comissão de Vendas", (CV * PV) / 100, CV],
+                ["F. Impostos", (IV * PV) / 100, IV],
+                ["G. Outros custos (D+E+F)", ((PV * CF) / 100)+((CV * PV) / 100)+((IV * PV) / 100), CF + CV + IV],
+                ["H. Rentabilidade (C-G)", ((PV - CP) - (((PV * CF) / 100) + ((CV * PV) / 100) + ((IV * PV) / 100))), ML]
+            ]
+            print(tabulate(tabela_resultados, headers = tabela_cabecalho))
 
             #Faixa de lucro do produto
+            rentabilidade = ((PV - CP) - (CF + CV + IV))
             if rentabilidade >= 0.20 * PV:
               print('sua classificação é de nivel alto')
               
