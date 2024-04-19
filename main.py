b000c924ee1d1607a64ecb53dd412f9024b39c4d
JOAO VICTOR DOS SANTOS ROCHA <joao.vsr1@puccampinas.edu.br>
2024-04-19 18:48:55 -0300

    Criação de funções para otimização

--- main.py
+++ main.py
@@ -1,43 +1,40 @@
-PK = input("digite o código do produto: ")
-nome_produto = input("digite o nome do produto: ")
-descrição_produto = input("digite a descrição do produto: ")
+def obter_input(mensagem):
+    valor = input(mensagem)
+    while not valor.strip():
+        print('\nINSIRA UM VALOR VÁLIDO!')
+        valor = input(mensagem)
+    return valor
+    
+def obter_num_float(mensagem):
+    valor = float(input(mensagem))
+    while valor <= 0:
+        print('\nINSIRA UM VALOR NUMÉRICO POSITIVO E ACIMA DE 0!')
+        valor = float(input(mensagem))
+    return valor
+
+print('SEJA BEM-VINDO AO INSTOCK!')
+print('PARA INICIARMOS FORNEÇA AS INFORMAÇÕES ABAIXO POR FAVOR\n')
+
+cod_produto = obter_input("Digite o código do produto: ") #chave primária
+nome_produto = obter_input("Digite o nome do produto: ")
+descricao_produto = obter_input("Digite a descrição do produto: ")
 
 while True:
         try:
             #custo do produto
-            CP = float(input("digite o custo do produto: "))
-            
-            if CP <= 0:
-                print("O custo do produto não pode ser menor ou igual a 0")
-                continue
-           
+            CP = obter_num_float("Digite o custo do produto (R$): ")
+               
             #custo fixo/administrativo
-            CF = float(input("digite o custo do fixo: "))
-            
-            if CF < 0:
-                print("O custo fixo não pode ser negativo!")
-                continue
-            
+            CF = obter_num_float("Digite o custo do fixo (%): ")
+                
             #comissão de vendas
-            CV = float(input("digite a comissão sobre a venda: "))
-            
-            if CV < 0:
-                print("A comissão de venda não pode ser negativa!")
-                continue
-            
+            CV = obter_num_float("Digite a comissão sobre a venda (%): ")
+                
             #impostos 
-            IV = float(input("digite o valor dos impostos: "))
-            
-            if IV < 0:
-                print("O valor dos impostos não pode ser negativo!")
-                continue
-            
+            IV = obter_num_float("Digite o valor dos impostos (%): ")
+                
             #rentabilidade
-            ML = float(input("digtite a rentabilidade desejada: "))
-            
-            if ML < 0:
-                print("A rentabilidade desejada não pode ser negativa!")
-                continue
+            ML = obter_num_float("Digite a rentabilidade desejada (%): ")
             
             #Fórmula Preço de Venda
             PV = CP / (1 - ((CF + CV + IV + ML) / 100))
